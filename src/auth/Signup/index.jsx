57c2d06d01d299f87ed08f3f4592cb9f11bc9e7d
Malbila <smalbila07@gmail.com>
2022-01-28 14:50:33 +0000

    Initial Commit

---
+++ src/auth/Signup/index.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+function Signup() {
+    const [textValue, setTextValue ] = useState('')
+    const [ passValue, setPassValue ] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log({passValue, textValue})
+    }
+
+    return (
+        <div>
+            <h1>Connexion</h1>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <input 
+                    type="text"
+                    placeholder="Name" 
+                    value={textValue}
+                    onChange={(e) => setTextValue(e.target.value)}
+                />
+                <input 
+                    type="password" 
+                    placeholder="Password" 
+                    value={passValue}
+                    onChange={(e) => setPassValue(e.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default Signup
---
+++ src/components/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Headers() {
+
+    return (
+        <div>
+            <h1>Headers</h1>
+        </div>
+    )
+}
+
+export default Headers
